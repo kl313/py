1721a64f27a853dc5fcd48ba3e9a9a352fc890bf
kl313 <uancc@foxmail.com>
2022-05-21 13:09:31 +0800

    uj newdef

---
+++ euclean.py
@@ -0,0 +1,32 @@
+import json
+from numpy import size
+import requests
+def req(size=10,lat=30.946805,lont=104.308798):
+    hds = {
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAwMTY3MTYsIm5hbWUiOiIxMzc4MzUzMTU5MCIsImFwcFVzZXJJZCI6MTQzMDM3MDcsImlhdCI6MTY0NzM0MjM1NywiZXhwIjoxNjU1Mzc3NTU3fQ.5pzDAEOQCp5ovDehaVzKHeRXDj8Es_mjSgUDWLRCFwc',
+    }
+
+    url = f'https://phoenix.ujing.online/api/v1/stores/near?page=1&size={size}&lat={lat}&lont={lont}&scope=2000&mode=BA' 
+    r = requests.get(url, headers=hds)
+    return r.text
+
+def filew():
+    f = open('uc.json','w')
+    f.write(req())
+    f.close()
+
+# 将 JSON 对象转换为 Python 字典
+filew()
+size=10
+with open('uc.json','r') as js:
+    data2 = json.loads(js.read())
+
+    lieb = data2['data']['storeList']  # 列表
+    for n in range(size):  # 遍历楼层
+        for j in lieb[n]:
+            if lieb[n]['id'] == '6039d48dd05d8b9662000020':
+                print(n)
+                a1 = lieb[n]['storeInfo'][0]['access']  # 【{}】
+                floor = lieb[n]['name']
+                print(f"{floor}可用洗衣机数量", a1)
+                break
---
+++ uclean.py
@@ -0,0 +1,30 @@
+import json
+import requests
+#from faker import Faker
+
+#fake = Faker()
+# with open('response.json', 'r', encoding='utf-8') as f:  #文件无需关闭
+hds = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4951.54 Safari/537.36 Edg/99.0.1210.39',
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAwMTY3MTYsIm5hbWUiOiIxMzc4MzUzMTU5MCIsImFwcFVzZXJJZCI6MTQzMDM3MDcsImlhdCI6MTY0NzM0MjM1NywiZXhwIjoxNjU1Mzc3NTU3fQ.5pzDAEOQCp5ovDehaVzKHeRXDj8Es_mjSgUDWLRCFwc',
+    'x-user-geo': '104.310345,30.946448',
+    'user-agent': 'okhttp/4.3.1'
+}
+
+url = 'https://phoenix.ujing.online/api/v1/stores/near?page=1&size=30&lat=30.946448&lont=104.310345&scope=2000&mode=BA h2'  # %f为位置坐标
+r = requests.get(url, headers=hds)
+
+
+# 将 JSON 对象转换为 Python 字典
+
+data2 = json.loads(r.text)
+
+lieb = data2['data']['storeList']  # 列表
+for n in range(30):  # 遍历楼层
+    for j in lieb[n]:
+        if lieb[n]['id'] == '6039d48dd05d8b9662000020':
+            print(n)
+            a1 = lieb[n]['storeInfo'][0]['access']  # 【{}】
+            floor = lieb[n]['name']
+            print(f"{floor}可用洗衣机数量", a1)
+            break
